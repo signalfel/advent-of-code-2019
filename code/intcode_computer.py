2c060aff44cce4c2b13f7a52c9521aeed7df7302
odebo@asus-rog <nodelynk@gmail.com>
2019-12-15 16:11:06 +0100

    quite major intcode computer updates. Works for programs up to day 9. Not ready.

--- code/intcode_computer.py
+++ code/intcode_computer.py
@@ -1,6 +1,4 @@
-
 from IPython.core.debugger import set_trace
-
 
 def state_from_program(program):
     return [int(e) for e in program.split(',')]
@@ -9,7 +7,8 @@ def get_intlist(integer):
     return [int(i) for i in list(str(integer))]
 
 class machine:
-    def __init__(self, program=None, inputs = None, return_output = False, verbose = True):
+    def __init__(self, program=None, inputs=None, 
+                 return_output=False, verbose=True):
         self.state = []
         self.head = 0
         self.__return_output = return_output
@@ -19,10 +18,7 @@ class machine:
         if program is not None:
             self.state = state_from_program(program)
 
-        if inputs is not None:
-            self.__inputs = inputs
-        else:
-            self.__inputs = None
+        self.__inputs = inputs
 
         if return_output:
             self.outputs = []
@@ -30,17 +26,17 @@ class machine:
         self.__paused = False
         self.is_terminated = False
 
-
       
     def run(self):
         if self.is_terminated:
-            print('Program has halted and cannot resume.')
+            print('Program has halted due to previous code 99 and cannot resume.')
         while not (self.__paused or self.is_terminated):
             self._opcode()
             
-            if self.state[self.head] == 99:
+            # if self.state[self.head] == 99:
+            if self._read_memory(self.head) == 99:
                 if self.__verbose:
-                    print('Termination criterion received. Halting.')
+                    print('Code 99 received. Halting.')
                 self.is_terminated = True
     
         if self.__return_output:
@@ -49,12 +45,54 @@ class machine:
     def input(self, inp):
         self.__inputs.append(inp)
         self.__paused = False
-                
+
+    def _read_memory(self, address, how = 'immediate'):
+        
+        if how == 'immediate':
+            add = address
+        elif how == 'positional':
+            add = self._read_memory(address, how = 'immediate')
+        elif how == 'relative':
+            add = self._read_memory(address, how = 'immediate') + self.__relative_base
+        else:
+            raise ValueError('Unknown parameter mode for reading: {}'.format(how))
+
+        try:
+            return self.state[add]
+        except IndexError:
+            self._extend_memory(add)
+            return self.state[add]
+
+    def _write_memory(self, address, value, how = 'positional'):
+
+        if how == 'immediate':
+            raise ValueError('Cannot write in immediateediate mode!')
+        elif how == 'positional':
+            add = address
+        elif how == 'relative':
+            add = address + self.__relative_base
+        else:
+            raise ValueError('Unknown parameter mode for writing: {}'.format(how))
+
+        try:
+            self.state[add] = value
+        except IndexError:
+            self._extend_memory(add)
+            self.state[add] = value
+
+    def _extend_memory(self, address):
+        if address < 0:
+            raise IndexError('Cannot access negative memory address.')
+
+        additional_memory = address - len(self.state) + 1
+        self.state += [0] * additional_memory
+
     def _opcode(self):
         # make a list of integers that defines the opcode, namely where
         # the instruction pointer (head) points in the state
-        code_seq = get_intlist(self.state[self.head])
-        
+        code_seq = get_intlist(self._read_memory(self.head))
+        # set_trace()
+
         # The actual opcode is the last digit
         code = code_seq[-1]
         
@@ -64,23 +102,24 @@ class machine:
         code_str_lengths = {1: 5,
                             2: 5, 
                             3: 1,
-                            4: 1,
+                            4: 3,
                             5: 4,
                             6: 4,
                             7: 5,
                             8: 5,
+                            9: 3,
                         }
         # If we expect a longer code string than we get, we add leading zeros
         while (len(code_seq) < code_str_lengths[code]):
             code_seq = [0] + code_seq
         
-        # part of the code signifies if the parameter type is position, immediate
+        # part of the code signifies if the parameter type is positional, immediate
         # or relative
-        parameter_map = {0: 'pos', 1: 'imm', 2: 'rel'}
+        parameter_map = {0: 'positional', 1: 'immediate', 2: 'relative'}
         
         
         # we figure out which parameters should be interpreted as what
-        par_types = [parameter_map[o] for o in reversed(code_seq[:-2])]
+        parameter_modes = [parameter_map[o] for o in reversed(code_seq[:-2])]
         
         # A mapping between op-codes and the functions we want to perform
         code_functions = {1: self._code_one, 
@@ -94,70 +133,35 @@ class machine:
                  9: self._code_nine,
                 }
         
-        # Some of the opcodes will require additional arguments, we can put them here.
-        # This seems more future-proof than a boolean
-        function_args = {1: {'par_types': par_types},
-                         2: {'par_types': par_types},
-                         3: {},
-                         4: {},
-                         5: {'par_types': par_types},
-                         6: {'par_types': par_types},
-                         7: {'par_types': par_types},
-                         8: {'par_types': par_types},
-                         9: {'par_types': par_types},
-                        }
         
         # We determine which function to apply, from the code received
         code_func = code_functions[code]
-        # What are the arguments we should provide?
-        args = function_args[code]
         
         # Apply the function. The functions themselves are responsible for altering the state
         # of the machine and moving the instruction pointer (head) appropriately
-        code_func(**args)
-
-    def _interpret_parameter(self, parameter_type, address):
-        # Opcodes have parameters sometimes, and this function gives values for those
-        # depending on if they are positional, immediate or relative (this information is stored
-        # in the input par_types)
-
-        # If we have immediate parameter type, we select the value in the state at the 
-        # parameter position. Otherwise, if we have positional parameter type and we select
-        # the value in the state at the address given by the parameter 
-        # Otherwise, we have a relative paramater type and we
-        if parameter_type is 'imm':
-            val = self.state[address]
-        elif parameter_type is 'pos':
-            val = self.state[self.state[address]]
-        elif parameter_type is 'rel':
-            val = self.state[self.state[address] + self.__relative_base]
-        else:
-            raise ValueError('Unknown parameter type: {}'.format(parameter_type))
-
-        return val
-    
-    
-    def _code_one(self, par_types):
+        code_func(parameter_modes)
+      
+    def _code_one(self, parameter_modes):
         # Add parameter 1 and 2 and store it at parameter 3
-        val1 = self._interpret_parameter(par_types[0], address=self.head + 1)
-        val2 = self._interpret_parameter(par_types[1], address=self.head + 2)
+        val1 = self._read_memory(self.head + 1, how=parameter_modes[0])
+        val2 = self._read_memory(self.head + 2, how=parameter_modes[1])
         
-        address = self.state[self.head+3]
+        address = self._read_memory(self.head + 3)
         
-        self.state[address] = val1 + val2
+        self._write_memory(address, val1 + val2)
         self.head += 4
         
-    def _code_two(self, par_types):
-        # Multiply parameter 1 and 2 and store it at parameter 3
-        val1 = self._interpret_parameter(par_types[0], address=self.head + 1)
-        val2 = self._interpret_parameter(par_types[1], address=self.head + 2)
+    def _code_two(self, parameter_modes):
+        # # Multiply parameter 1 and 2 and store it at parameter 3
+        val1 = self._read_memory(self.head + 1, how=parameter_modes[0])
+        val2 = self._read_memory(self.head + 2, how=parameter_modes[1])
         
-        address = self.state[self.head+3]
+        address = self._read_memory(self.head + 3)
         
-        self.state[address] = val1 * val2
+        self._write_memory(address, val1 * val2)
         self.head += 4
         
-    def _code_three(self):    
+    def _code_three(self, parameter_modes):    
         # Take an input number and store it at the address given by
         # parameter 1
         if self.__inputs == []:
@@ -173,62 +177,63 @@ class machine:
                     break
             else:
                 val = self.__inputs.pop(0)
-
-            address = self.state[self.head+1]
-            self.state[address] = val
+            # set_trace()
+            address = self._read_memory(self.head + 1, how = parameter_modes[0])
+            self._write_memory(address, val)
             self.head += 2
         
-    def _code_four(self):
-        # Only output the value stored positionally at the code's 
-        # parameter
-        address = self.state[self.head+1]
-        output = self.state[address]
+    def _code_four(self, parameter_modes):
+        # Only output the value given by the code's parameter
+        output = self._read_memory(self.head+1, how=parameter_modes[0])
+
         if self.__return_output:
             self.outputs.append(output)      
         if self.__verbose:
             print('Output: {}'.format(output))
         self.head += 2
     
-    def _code_five(self, par_types):     
+    def _code_five(self, parameter_modes):     
         # If parameter 1 is non-zero, set head at parameter 2
-        val1 = self._interpret_parameter(par_types[0], address=self.head + 1)
-        val2 = self._interpret_parameter(par_types[1], address=self.head + 2)
+        val1 = self._read_memory(self.head + 1, how=parameter_modes[0])
+        val2 = self._read_memory(self.head + 2, how=parameter_modes[1])
 
         if val1 != 0:
             self.head = val2
         else:
             self.head += 3
             
-    def _code_six(self, par_types):     
+    def _code_six(self, parameter_modes):     
         # If parameter 1 is zero, set head at parameter 2
-        val1 = self._interpret_parameter(par_types[0], address=self.head + 1)
-        val2 = self._interpret_parameter(par_types[1], address=self.head + 2)
+        val1 = self._read_memory(self.head + 1, how=parameter_modes[0])
+        val2 = self._read_memory(self.head + 2, how=parameter_modes[1])
         
         if val1 == 0:
             self.head = val2
         else:
             self.head += 3
     
-    def _code_seven(self, par_types):
+    def _code_seven(self, parameter_modes):
         # If parameter 1 is less than parameter 2, set 1 at parameter 3 else 0 
-        val1 = self._interpret_parameter(par_types[0], address=self.head + 1)
-        val2 = self._interpret_parameter(par_types[1], address=self.head + 2)
+        val1 = self._read_memory(self.head + 1, how=parameter_modes[0])
+        val2 = self._read_memory(self.head + 2, how=parameter_modes[1])
 
-        address = self.state[self.head+3]
-        self.state[address] = 1 if val1 < val2 else 0
+        address = self._read_memory(self.head + 3)
+
+        self._write_memory(address, 1 if val1 < val2 else 0)
         self.head += 4
     
-    def _code_eight(self, par_types):
+    def _code_eight(self, parameter_modes):
         # If parameter 1 equals parameter 2, set 1 at parameter 3 else 0 
-        val1 = self._interpret_parameter(par_types[0], address=self.head + 1)
-        val2 = self._interpret_parameter(par_types[1], address=self.head + 2)
+        val1 = self._read_memory(self.head + 1, how=parameter_modes[0])
+        val2 = self._read_memory(self.head + 2, how=parameter_modes[1])
 
-        address = self.state[self.head+3]
-        self.state[address] = 1 if val1 == val2 else 0
+        address = self._read_memory(self.head + 3)
+        self._write_memory(address, 1 if val1 == val2 else 0)
         self.head += 4
     
-    def _code_nine(self, par_types):
-        val = self._interpret_parameter(par_types[0], address=self.head + 1)
+    def _code_nine(self, parameter_modes):
+        # increase/decrease the relative base by the code's parameter
+        val = self._read_memory(self.head + 1, how=parameter_modes[0])
         self.__relative_base += val
         self.head += 2
 
